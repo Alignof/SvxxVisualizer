d71a9f93b54c3817f58fffaad14fcfae36c1e875
Alingof <Alignof@outlook.com>
2023-10-26 02:57:40 +0900

    [update] grouping form and bit field

--- src/visualizer.rs
+++ src/visualizer.rs
@@ -6,28 +6,31 @@ pub fn visualizer(cx: Scope) -> Element {
     let vaddr = use_state(cx, || 0);
     cx.render(rsx! {
         div {
-            class: "flex space-x-3 p-2",
-            p {
-                class: "float-left",
-                "vaddr:"
-            }
+            class: "mx-auto p-8 flex flex-col justify-start",
+            div {
+                class: "flex space-x-3 py-2",
+                p {
+                    class: "float-left text-lg",
+                    "vaddr:"
+                }
 
-            form {
-                onsubmit: |_| {},
-                input {
-                    class: "bg-gray-900",
-                    oninput: move |event|
-                    if let Some(hex_noprefix) = event.value.strip_prefix("0x") {
-                        if let Ok(hex) = u64::from_str_radix(hex_noprefix, 16) {
-                            vaddr.set(hex);
+                form {
+                    onsubmit: |_| {},
+                    input {
+                        class: "bg-gray-900",
+                        oninput: move |event|
+                        if let Some(hex_noprefix) = event.value.strip_prefix("0x") {
+                            if let Ok(hex) = u64::from_str_radix(hex_noprefix, 16) {
+                                vaddr.set(hex);
+                            }
+                        } else if let Ok(dec) = event.value.parse::<u64>() {
+                            vaddr.set(dec);
                         }
-                    } else if let Ok(dec) = event.value.parse::<u64>() {
-                        vaddr.set(dec);
                     }
                 }
             }
-        }
 
-        bit_field::vaddr(cx, vaddr)
+            bit_field::vaddr(cx, vaddr)
+        }
     })
 }
