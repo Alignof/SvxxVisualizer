628049e159f834dfd449b886bf682e7017fe7094
Alingof <Alignof@outlook.com>
2023-11-13 16:03:17 +0900

    [add] add a sturct `TranslateState` to visualizer.rs

--- src/visualizer.rs
+++ src/visualizer.rs
@@ -4,12 +4,45 @@ use dioxus::prelude::*;
 mod pte;
 mod vaddr;
 
+#[derive(Clone)]
+struct TranslateState {
+    vpn: [u32; 3],
+    ppn: [u32; 3],
+    level_flags: [bool; 3],
+}
+
+impl TranslateState {
+    pub fn new() -> Self {
+        TranslateState {
+            vpn: [0, 0, 0],
+            ppn: [0, 0, 0],
+            level_flags: [true, false, false],
+        }
+    }
+
+    pub fn init_vpn(&mut self, vpn_value: u64) {
+        self.vpn[0] = vpn_value as u32 >> 12 & 0x1ff;
+        self.vpn[1] = vpn_value as u32 >> 21 & 0x1ff;
+        self.vpn[2] = vpn_value as u32 >> 30 & 0x1ff;
+    }
+
+    pub fn vpn(&self, index: usize) -> u64 {
+        self.vpn[index] as u64
+    }
+
+    pub fn set_ppn(&mut self, ppn_value: u64) {
+        self.ppn[0] = ppn_value as u32 >> 10 & 0x1ff;
+        self.ppn[1] = ppn_value as u32 >> 19 & 0x1ff;
+        self.ppn[2] = ppn_value as u32 >> 28 & 0x3ffffff;
+    }
+}
+
 pub fn visualizer(cx: Scope) -> Element {
     let conf = use_shared_state::<Config>(cx).unwrap();
     let vaddr = use_state(cx, || 0);
-    let vpn_2 = use_state(cx, || 0);
+    let trans_state = use_state(cx, || TranslateState::new());
     let pte_addr_1 = use_state(cx, || 0);
-    let pte = use_state(cx, || 0);
+    let pte_1 = use_state(cx, || 0);
     cx.render(rsx! {
         div {
             class: "mx-auto p-8 flex flex-col justify-start",
@@ -28,13 +61,13 @@ pub fn visualizer(cx: Scope) -> Element {
                         if let Some(hex_noprefix) = event.value.strip_prefix("0x") {
                             if let Ok(hex) = u64::from_str_radix(hex_noprefix, 16) {
                                 vaddr.set(hex);
-                                vpn_2.set(hex >> 30 & 0x1ff);
-                                pte_addr_1.set(conf.read().satp_ppn as u64 * 4096 + vpn_2.get() * 8);
+                                trans_state.with_mut(|t| t.init_vpn(hex));
+                                pte_addr_1.set(conf.read().satp_ppn * 4096 + trans_state.get().vpn(2) * 8);
                             }
                         } else if let Ok(dec) = event.value.parse::<u64>() {
                             vaddr.set(dec);
-                            vpn_2.set(dec >> 30 & 0x1ff);
-                            pte_addr_1.set(conf.read().satp_ppn as u64 * 4096 + vpn_2.get() * 8);
+                            trans_state.with_mut(|t| t.init_vpn(dec));
+                            pte_addr_1.set(conf.read().satp_ppn * 4096 + trans_state.get().vpn(2) * 8);
                         }
                     }
                 }
@@ -47,7 +80,7 @@ pub fn visualizer(cx: Scope) -> Element {
         div {
             class: "mx-auto px-8 flex flex-col justify-start",
 
-            pte::pte_addr(cx, conf.read().satp_ppn, *vpn_2.get())
+            pte::pte_addr(cx, conf.read().satp_ppn, trans_state.get().vpn(2))
 
             div {
                 class: "flex space-x-3 py-2",
@@ -63,17 +96,19 @@ pub fn visualizer(cx: Scope) -> Element {
                         oninput: move |event|
                         if let Some(hex_noprefix) = event.value.strip_prefix("0x") {
                             if let Ok(hex) = u64::from_str_radix(hex_noprefix, 16) {
-                                pte.set(hex)
+                                pte_1.set(hex);
+                                trans_state.with_mut(|t| t.set_ppn(hex));
                             }
                         } else if let Ok(dec) = event.value.parse::<u64>() {
-                            pte.set(dec)
+                            pte_1.set(dec);
+                            trans_state.with_mut(|t| t.set_ppn(dec));
                         }
                     }
                 }
             }
 
-            pte::bit_field(cx, pte)
-            pte::pte_data(cx, pte)
+            pte::bit_field(cx, pte_1)
+            pte::pte_data(cx, pte_1)
         }
     })
 }
