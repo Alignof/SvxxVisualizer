8860750c0bbb1d01292e1ddfeef7e17dc413f4b3
Alingof <Alignof@outlook.com>
2024-02-25 15:04:50 +0900

    [add] add vaddr to TranslateState

--- src/visualizer.rs
+++ src/visualizer.rs
@@ -7,6 +7,8 @@ mod vaddr;
 /// Global state
 #[derive(Clone)]
 struct TranslateState {
+    /// Virtual address.
+    vaddr: u64,
     /// Virtual page number.
     vpn: [u32; 3],
     /// Physical page number.
@@ -21,11 +23,22 @@ impl TranslateState {
     /// Constructor.
     pub fn new() -> Self {
         TranslateState {
+            vaddr: 0,
             vpn: [0, 0, 0],
             ppn: [0, 0, 0],
             level_flags: [false, false, true],
             result_flag: false,
         }
+    }
+
+    /// Set vaddr.
+    pub fn set_vaddr(&mut self, vaddr: u64) {
+        self.vaddr = vaddr;
+    }
+
+    /// Get vaddr.
+    pub fn get_vaddr(&self) -> u64 {
+        self.vaddr
     }
 
     /// Set VPN from the vpn_value.
@@ -241,12 +254,8 @@ fn trans_lv3<'a>(
     })
 }
 
-fn show_paddr<'a>(
-    cx: Scope<'a>,
-    page_off: u64,
-    vaddr: &'a UseState<u64>,
-    trans_state: &'a UseState<TranslateState>,
-) -> Element<'a> {
+fn show_paddr<'a>(cx: Scope<'a>, trans_state: &'a UseState<TranslateState>) -> Element<'a> {
+    let page_off = trans_state.get_vaddr() & 0xfff;
     let level = trans_state.get().get_level();
     let trans = trans_state.get();
     cx.render(rsx! {
@@ -256,23 +265,23 @@ fn show_paddr<'a>(
             div {
                 class: "p-4 text-xl bg-red-400",
                 p {
-                    format!("vaddr: {:#x}", vaddr.get())
+                    format!("vaddr: {:#x}", trans_state.get_vaddr())
                 }
                 p {
-                    match level {
-                        0 => format!("→ paddr: {:#x}", trans.ppn(2) << 30 | trans.ppn(1) << 21 | trans.ppn(0) << 12 | page_off),
-                        1 => {
+                    match level + 1 {
+                        1 => format!("→ paddr: {:#x}", trans.vpn(2) << 30 | trans.ppn(1) << 21 | trans.ppn(0) << 12 | page_off),
+                        2 => {
                             if trans.ppn(0) != 0 {
                                 "trans.ppn(0) != 0".to_string()
                             } else {
-                                format!("→ paddr: {:#x}", trans.ppn(2) << 30 | trans.ppn(1) << 21 | trans.vpn(0) << 12 | page_off)
+                                format!("→ paddr: {:#x}", trans.vpn(2) << 30 | trans.vpn(1) << 21 | trans.ppn(0) << 12 | page_off)
                             }
                         }
-                        2 => {
+                        3 => {
                             if trans.ppn(0) != 0 || trans.ppn(1) != 0 {
                                 "trans.ppn(0) != 0 || trans.ppn(1) != 0".to_string()
                             } else {
-                                format!("→ paddr: {:#x}", trans.ppn(2) << 30 | trans.vpn(1) << 21 | trans.vpn(0) << 12 | page_off)
+                                format!("→ paddr: {:#x}", trans.vpn(2) << 30 | trans.vpn(1) << 21 | trans.vpn(0) << 12 | page_off)
                             }
                         }
                         _ => String::new()
@@ -286,7 +295,6 @@ fn show_paddr<'a>(
 /// Show address translation visualizer.
 pub fn visualizer(cx: Scope) -> Element {
     let conf = use_shared_state::<Config>(cx).unwrap();
-    let vaddr = use_state(cx, || 0);
     let trans_state = use_state(cx, TranslateState::new);
     let pte_addr_1 = use_state(cx, || 0);
     let pte_1 = use_state(cx, || 0);
@@ -311,12 +319,12 @@ pub fn visualizer(cx: Scope) -> Element {
                         oninput: move |event|
                         if let Some(hex_noprefix) = event.value.strip_prefix("0x") {
                             if let Ok(hex) = u64::from_str_radix(hex_noprefix, 16) {
-                                vaddr.set(hex);
+                                trans_state.with_mut(|t| t.set_vaddr(hex));
                                 trans_state.with_mut(|t| t.set_vpn(hex));
                                 pte_addr_1.set(conf.read().satp_ppn * 4096 + trans_state.get().vpn(2) * 8);
                             }
                         } else if let Ok(dec) = event.value.parse::<u64>() {
-                            vaddr.set(dec);
+                            trans_state.with_mut(|t| t.set_vaddr(dec));
                             trans_state.with_mut(|t| t.set_vpn(dec));
                             pte_addr_1.set(conf.read().satp_ppn * 4096 + trans_state.get().vpn(2) * 8);
                         }
@@ -324,8 +332,8 @@ pub fn visualizer(cx: Scope) -> Element {
                 }
             }
 
-            vaddr::bit_field(cx, vaddr)
-            vaddr::bit_data(cx, vaddr)
+            vaddr::bit_field(cx, trans_state)
+            vaddr::bit_data(cx, trans_state)
         }
 
         if trans_state.get().flags(2) {
@@ -341,7 +349,7 @@ pub fn visualizer(cx: Scope) -> Element {
         }
 
         if trans_state.get().result_flag() {
-            show_paddr(cx, vaddr.get() & 0xfff, vaddr, trans_state)
+            show_paddr(cx, trans_state)
         }
     })
 }
