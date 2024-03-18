df1f642e33be316f82fd8c2b0a0fab5bf220533a
Alignof <Alignof@users.noreply.github.com>
2024-03-18 14:54:27 +0000

    deploy: 7ed11849ee3ec8f3e2fd976aea07d50d40078ed8

--- snippets/dioxus-interpreter-js-85a0a2f8acce2e86/inline0.js
+++ snippets/dioxus-interpreter-js-85a0a2f8acce2e86/inline0.js
@@ -169,7 +169,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
       function truthy(val) {
         return val === "true" || val === true;
       }
-    const attr = [];
+    let u32buf,u32bufp;let u8buf,u8bufp;const attr = [];
                     let attr_tmp1, attr_tmp2;
                     function get_attr() {
                         attr_tmp2 = u8buf[u8bufp++];
@@ -193,7 +193,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         else{
                             return ns_cache[ns_cache_tmp2&4294967167];
                         }
-                    }let u32buf,u32bufp;let u8buf,u8bufp;const evt = [];
+                    }const evt = [];
                     let evt_tmp1, evt_tmp2;
                     function get_evt() {
                         evt_tmp2 = u8buf[u8bufp++];
@@ -206,7 +206,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                             return evt[evt_tmp2&4294967167];
                         }
                     }let s = "";let lsp,sp,sl; let c = new TextDecoder();
-            let event_name,id,ptr,bubbles,field,ns,len,value;
+            let value,len,bubbles,event_name,field,ns,id,ptr;
             export function create(r){
                 d=r;
             }
